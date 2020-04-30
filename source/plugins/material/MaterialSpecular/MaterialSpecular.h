9fe7f0d0d6abb423361e71c97acc20b9ccbc854d
v.grigoriev <v.grigoriev@cyi.ac.cy>
2020-04-30 17:02:17 +0300

    Default material revised

---
+++ source/plugins/material/MaterialSpecular/MaterialSpecular.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <Inventor/fields/SoSFDouble.h>
+#include <Inventor/fields/SoSFEnum.h>
+#include <Inventor/fields/SoSFFloat.h>
+#include <Inventor/fields/SoSFString.h>
+
+#include "kernel/material/MaterialAbstract.h"
+#include "kernel/TonatiuhTypes.h"
+
+class SoSensor;
+class SoFieldSensor;
+
+
+class MaterialSpecular: public MaterialAbstract
+{
+    SO_NODE_HEADER(MaterialSpecular);
+
+public:
+    enum Distribution {
+        //PILLBOX = 0,
+        NORMAL = 1,
+    };
+
+    MaterialSpecular();
+	static void initClass();
+
+    bool OutputRay(const Ray& incident, DifferentialGeometry* dg, RandomAbstract& rand, Ray* outputRay) const;
+
+	trt::TONATIUH_REAL m_reflectivity;
+	trt::TONATIUH_REAL m_sigmaSlope;
+	SoSFEnum m_distribution;
+
+    SoMFColor m_ambientColor;
+    SoMFColor m_diffuseColor;
+    SoMFColor m_specularColor;
+    SoMFColor m_emissiveColor;
+	SoMFFloat m_shininess;
+    SoMFFloat m_transparency;
+
+    NAME_ICON_FUNCTIONS("Specular", ":/MaterialSpecular.png")
+
+protected:
+    ~MaterialSpecular();
+
+    double m_sigmaOpt;
+
+    static void updateReflectivity(void* data, SoSensor*);
+    static void updateAmbientColor(void* data, SoSensor*);
+    static void updateDiffuseColor(void* data, SoSensor*);
+    static void updateSpecularColor(void* data, SoSensor*);
+    static void updateEmissiveColor(void* data, SoSensor*);
+    static void updateShininess(void* data, SoSensor*);
+    static void updateTransparency(void* data, SoSensor*);
+
+private:
+    SoFieldSensor* m_reflectivitySensor;
+    SoFieldSensor* m_ambientColorSensor;
+    SoFieldSensor* m_diffuseColorSensor;
+    SoFieldSensor* m_specularColorSensor;
+    SoFieldSensor* m_emissiveColorSensor;
+    SoFieldSensor* m_shininessSensor;
+    SoFieldSensor* m_transparencySensor;
+};
+
+
+
+#include "kernel/material/MaterialFactory.h"
+
+class MaterialSpecularFactory:
+    public QObject, public MaterialFactoryT<MaterialSpecular>
+{
+    Q_OBJECT
+    Q_INTERFACES(MaterialFactory)
+    Q_PLUGIN_METADATA(IID "tonatiuh.MaterialFactory")
+};
